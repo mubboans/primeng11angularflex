ea28a38f3c713a239c2343eb91217820a9808409
fy5mubashir <mubboans007@gmail.com>
2022-09-22 18:21:26 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,116 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MenubarModule} from 'primeng-lts/menubar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import {TooltipModule} from 'primeng-lts/tooltip'; 
+
+import {CarouselModule} from 'primeng-lts/carousel';
+import {DynamicDialogModule} from 'primeng-lts/dynamicdialog';
+import {ToastModule} from 'primeng-lts/toast';
+import {CalendarModule} from 'primeng-lts/calendar';
+import {SliderModule} from 'primeng-lts/slider';
+import {ContextMenuModule} from 'primeng-lts/contextmenu';
+import {DialogModule} from 'primeng-lts/dialog';
+import {ButtonModule} from 'primeng-lts/button';
+import {ProgressBarModule} from 'primeng-lts/progressbar';
+import {InputTextModule} from 'primeng-lts/inputtext';
+import {CheckboxModule} from 'primeng-lts/checkbox';
+import {FileUploadModule} from 'primeng-lts/fileupload';
+import {ToolbarModule} from 'primeng-lts/toolbar';
+import {RatingModule} from 'primeng-lts/rating';
+import {RadioButtonModule} from 'primeng-lts/radiobutton';
+import {InputNumberModule} from 'primeng-lts/inputnumber';
+import { ConfirmDialogModule } from 'primeng-lts/confirmdialog';
+import {InputSwitchModule} from 'primeng-lts/inputswitch';
+import { InputTextareaModule } from 'primeng-lts/inputtextarea'; 
+import { SelectButtonModule } from 'primeng-lts/selectbutton';
+import {TagModule} from 'primeng-lts/tag';
+import {InputMaskModule} from 'primeng-lts/inputmask';
+import {CardModule} from 'primeng-lts/card';
+import {RippleModule} from 'primeng-lts/ripple';
+import {AvatarModule} from 'primeng-lts/avatar';
+import {TabViewModule} from 'primeng-lts/tabview';
+import { TimelineModule } from "primeng-lts/timeline";
+
+import {ProgressSpinnerModule} from 'primeng-lts/progressspinner';
+
+import {ToggleButtonModule} from 'primeng-lts/togglebutton';
+import {AccordionModule} from 'primeng-lts/accordion';
+import {TieredMenuModule} from 'primeng-lts/tieredmenu';
+import {FieldsetModule} from 'primeng-lts/fieldset';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MenubarModule,
+    TieredMenuModule,
+    ButtonModule,
+    FieldsetModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    InputSwitchModule,
+
+
+    CardModule,
+
+    CarouselModule,
+    TooltipModule,
+
+
+    CalendarModule,
+		SliderModule,
+		DialogModule,
+
+		ContextMenuModule,
+
+		ButtonModule,
+		ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    CheckboxModule,    
+    FileUploadModule,
+    ToolbarModule,
+    RatingModule,
+    FormsModule,
+    RadioButtonModule,
+    InputNumberModule,
+    ConfirmDialogModule,
+    InputTextareaModule,
+    SelectButtonModule,
+    TagModule,
+    InputMaskModule,
+    DynamicDialogModule,
+    RippleModule,
+
+    AvatarModule,
+    TabViewModule,
+    TimelineModule,
+
+    ProgressSpinnerModule,
+    ToggleButtonModule,
+    AccordionModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
